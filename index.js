291e631fe57ff5bf7523be081b93f8da77b88e8b
Divya <dgoel27@yahoo.in>
2018-04-30 22:31:06 +0530

    fourth commit

--- index.js
+++ index.js
@@ -2,37 +2,37 @@ $(document).ready(() => {
 
     $(".collapseBtn").hover(() => {
 
-        $(".collapseBtn").css("backgroundColor", "#154360");
+        $(".collapseBtn").css("backgroundColor", "#2E4053");
 
     }) 
 
     $(".collapseBtn").mouseout(() => {
 
-        $(".collapseBtn").css("backgroundColor", "#4A235A");
+        $(".collapseBtn").css("backgroundColor", "#1A5276");
 
     }) 
 
     $(".NavBtn").hover(() => {
 
-        $(".NavBtn").css("backgroundColor", "#154360");
+        $(".NavBtn").css("backgroundColor", "#2E4053");
 
     }) 
 
     $(".NavBtn").mouseout(() => {
 
-        $(".NavBtn").css("backgroundColor", "#4A235A");
+        $(".NavBtn").css("backgroundColor", "#1A5276");
 
     })
 
     $(".modalButton").hover(() => {
 
-        $(".modalButton").css("backgroundColor", "#154360");
+        $(".modalButton").css("backgroundColor", "#2E4053");
 
     }) 
 
     $(".modalButton").mouseout(() => {
 
-        $(".modalButton").css("backgroundColor", "#4A235A");
+        $(".modalButton").css("backgroundColor", "#1A5276");
 
     })
 
